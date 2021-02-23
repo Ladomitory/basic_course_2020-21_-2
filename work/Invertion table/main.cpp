0ab30c4970d3aaa17f92c3b7f03b75f4327cab9d
Ladomitory <radomirholmogorov@yandex.ru>
2021-02-23 16:08:03 +0700

    123

--- work/Invertion table/main.cpp
+++ work/Invertion table/main.cpp
@@ -5,8 +5,10 @@ using namespace std;
 
 int main()
 {
-    string s;
-    cin >> s;
+    vector <int> s;
+    int n; cin >> n;
+    for (int i = 0; i < n; ++i)
+        cin >> s[i];
     vector <int> ans;
     for (int j = s.size() - 1; j >= 0; --j)
     {
