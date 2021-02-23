0bf22789ae438a653da139ed3ac5d0baf829d166
Ladomitory <radomirholmogorov@yandex.ru>
2021-02-23 18:51:31 +0700

    123

--- work/Invertion table/main.cpp
+++ work/Invertion table/main.cpp
@@ -5,25 +5,38 @@ using namespace std;
 
 int main()
 {
-    vector <int> s;
-    int n; cin >> n;
+    //coding
+    int n; 
+    vector <int> s(n);
     for (int i = 0; i < n; ++i)
         cin >> s[i];
     vector <int> ans;
-    for (int j = s.size() - 1; j >= 0; --j)
+    for (int i = 0; i < n; ++i)
     {
-        int c, cc = s[j];
-        s[j] = j + 1;
-        for (int i = j + 1; i < s.size(); ++i)
+        
+    }
+
+    //decoding
+    int n; cin >> n;
+    vector <int> t(n);
+    for (int i = 0; i < n; ++i)
+        cin >> t[i];
+    vector <int> ans;
+    for (int j = n - 1; j >= 0; --j)
+    {
+        int c, cc = t[j];
+        t[j] = j + 1;
+        cout << j + 1;
+        for (int i = j + 1; i < n; ++i)
         {
-            c = s[i];
-            s[i] = cc;
+            c = t[i];
+            t[i] = cc;
             cc = c;
             c = '\0';
         }
-        s.push_back(cc);
+        t.push_back(cc);
     }
-    for (int i = 0; i < ans.size(); ++i)
+    for (int i = 0; i < n; ++i)
         cout << ans[i];
     cout << endl;
     return 0;
