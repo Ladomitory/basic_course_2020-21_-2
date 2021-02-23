2dd6b54b9519e611f56a5c46e43f2017e9b657a3
Ladomitory <radomirholmogorov@yandex.ru>
2021-02-23 19:08:23 +0700

    123

--- work/Invertion table/main.cpp
+++ work/Invertion table/main.cpp
@@ -5,29 +5,44 @@ using namespace std;
 
 int main()
 {
+/*
     //coding
     int n; 
+    cin >> n;
     vector <int> s(n);
     for (int i = 0; i < n; ++i)
         cin >> s[i];
-    vector <int> ans;
+    vector <int> ans(n, 0);
     for (int i = 0; i < n; ++i)
     {
-        
+        int a = 0;
+        for (int j = 0; j < n; ++j)
+        {
+            if (s[j] == ++i)
+            {
+                ans[i] = a;
+                break;
+            }
+            else if (s[j] > ++i)
+                ++a;
+        }
     }
-
-    //decoding
-    int n; cin >> n;
-    vector <int> t(n);
     for (int i = 0; i < n; ++i)
+        cout << ans[i] << ' ';
+    cout << endl;
+*/
+    //decoding
+    int m; cin >> m;
+    vector <int> t(m);
+    for (int i = 0; i < m; ++i)
         cin >> t[i];
-    vector <int> ans;
-    for (int j = n - 1; j >= 0; --j)
+    vector <int> anss;
+    for (int j = m - 1; j >= 0; --j)
     {
         int c, cc = t[j];
         t[j] = j + 1;
         cout << j + 1;
-        for (int i = j + 1; i < n; ++i)
+        for (int i = j + 1; i < m; ++i)
         {
             c = t[i];
             t[i] = cc;
@@ -36,8 +51,8 @@ int main()
         }
         t.push_back(cc);
     }
-    for (int i = 0; i < n; ++i)
-        cout << ans[i];
+    for (int i = 0; i < m; ++i)
+        cout << anss[i] << '\t';
     cout << endl;
     return 0;
 }
