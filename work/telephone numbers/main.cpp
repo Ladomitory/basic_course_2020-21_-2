80076ed202ac69a20ccf87990a2126da03834159
Ladomitory <radomirholmogorov@yandex.ru>
2021-02-25 19:23:44 +0700

    123

--- work/telephone numbers/main.cpp
+++ work/telephone numbers/main.cpp
@@ -1,21 +1,114 @@
+// mix the company with numbers
 #include <iostream>
-#include <fstream>
+#include <string>
 #include <map>
+#include <cmath>
+#include <algorithm>
+#include <vector>
+#define PRIORITY 0
 
 using namespace std;
 
+void print_vec(int n, int m, vector <vector <int> > v)
+{
+    for(int i = 0; i < n; ++i)
+    {
+        for(int j = 0; j < m; ++j)
+			cout << v[i][j] << " ";
+		cout << endl;
+    }
+}
+
+map <int, string> alf {{2, "ABC"}, {3, "DEF"}, {4, "GHI"}, {5, "JKL"}, {6, "MN"}, {7, "PRS"}, {8, "TUV"}, {9, "WXY"}, {0, "OQZ"}};
+
 int main()
 {
-    ifstream datafile("C:\\Users\\radomir\\proga\\informatic\\basic course 2020-21  2\\work\\telphone numbers\\database.txt");
+    string name;
+    cin >> name;
     
-    map <int, string> mp;
-    int n; string s;
-    for (int i = 0; i < 10; ++i)
+    string num;
+    cin >> num;
+    
+    int n1 = name.size() + 1;
+    int n2 = num.size() + 1;
+
+    vector <vector <int> > arr(n1, vector <int> (n2));
+    
+    for (int i = 0; i < n1; ++i)
+        arr[i][0] = i;
+ 
+    for (int i = 0; i < n2; ++i)
+        arr[0][i] = i;
+ 
+    for (int i = 1; i < n1; ++i)
+        for (int j = 1; j < n2; ++j)
+        {
+            if (alf[num[i - 1] - '0'].find(name[j - 1]) != string::npos)
+                arr[i][j] = min(min(arr[i - 1][j] + 1, arr[i][j - 1] + 1), arr[i - 1][j - 1]);
+	        else
+	            arr[i][j] = min(arr[i - 1][j] + 1, arr[i][j - 1] + 1);
+        }
+
+    print_vec(n1, n2, arr);
+    
+    int i = n1 - 1, j = n2 - 1;
+
+    while (i >= 0 && j >= 0)
     {
-        datafile >> n;
-        datafile >> s;
-        
+        if (PRIORITY)
+        {
+            if (arr[i][j] - arr[max(0, i - 1)][j] == 1)
+            {
+                cout << "INSERT '" << num[i - 1] << "' before " << j << endl;
+                cout << "\tBEFORE: " << name << endl;
+
+                name.insert(j, 1, num[i - 1]);
+                --i;
+                
+                cout << "\tAFTER: " << name << endl;
+            } else if (arr[i][j] - arr[i][max(j - 1, 0)] == 1)
+            {
+                cout << "DELETE '" << name[j - 1] << "' at " << j - 1 << endl;
+                cout << "\tBEFORE: " << name << endl;
+
+                name.erase(j - 1, 1);
+                --j; 
+
+                cout << "\tAFTER: " << name << endl;
+            } else
+            {
+                --i;
+                --j;
+            }
+        } else 
+        {
+            if (arr[i][j] - arr[i][max(j - 1, 0)] == 1)
+            {
+                cout << "DELETE '" << name[j - 1] << "' at " << j - 1 << endl;
+                cout << "\tBEFORE: " << name << endl;
+
+                name.erase(j - 1, 1);
+                --j; 
+
+                cout << "\tAFTER: " << name << endl;
+            } else if (arr[i][j] - arr[max(0, i - 1)][j] == 1)
+            {
+                cout << "INSERT '" << num[i - 1] << "' before " << j << endl;
+                cout << "\tBEFORE: " << name << endl;
+
+                name.insert(j, 1, num[i - 1]);
+                --i;
+                
+                cout << "\tAFTER: " << name << endl;
+            } else
+            {
+                i--;
+                j--;
+            }
+        }
     }
-    
+
+    cout << name << endl;
+	cout << arr[n1 - 1][n2 - 1] << endl;
     return 0;
 }
