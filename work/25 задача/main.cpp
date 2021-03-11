9130d4ff843170bf408998526e8b08bbd3b15b6d
Ladomitory <radomirholmogorov@yandex.ru>
2021-03-11 14:13:56 +0700

    25

--- work/25 задача/main.cpp
+++ work/25 задача/main.cpp
@@ -1,11 +1,16 @@
 #include <iostream>
+#include <vector>
+
 
 using namespace std;
+
+
 
 void f(int l, int r)
 {
     for (int i = l; i <= r; ++i)
     {
+        vector <int> a(4, 0);
         
     }
 }
