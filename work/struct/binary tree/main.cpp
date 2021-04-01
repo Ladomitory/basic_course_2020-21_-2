53daa95fcd1af5629f25dfdbb12c0e26c83ad910
Ladomitory <radomirholmogorov@yandex.ru>
2021-04-01 12:50:41 +0700

    123

--- work/struct/binary tree/main.cpp
+++ work/struct/binary tree/main.cpp
@@ -1,17 +1,62 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
-class tree
+class infix_tree
 {
     private:
         struct node
         {
-            int value;
+            string str = "\0";
             struct node *left = NULL;
             struct node *right = NULL;
         };
         struct node *root;
     public:
-        void a();
+        infix_tree ();
+        
+        void push(string str)
+        {
+            if (this->empty())
+            {
+                struct node * n /*new*/ = (node*) malloc(sizeof(node));
+                n->str = str;
+                this->root = n;  
+            }
+            struct node * curr /*current*/ = this->root;
+            struct node * n /*new*/ = (node*) malloc(sizeof(node));
+            n->str = str;
+            while (curr)
+            {
+                if (curr->str > n->str)
+                {
+                    if (curr->right)
+                        curr = curr->right;
+                    else
+                    {
+                        curr->right = n;
+                        break;
+                    } 
+                }
+                else if (curr->str < n->str)
+                {
+                    if (curr->left)
+                        curr = curr->left;
+                    else
+                    {
+                        curr->left = n;
+                        break;
+                    }
+                }
+                else //curr->str == n->str
+                    break;
+            }
+            return;
+        }
+
+        bool empty()
+        {
+            return (this->root == NULL);
+        }
 };
